66333d8a4b5a96e468558e1b4fde31794facf9d8
Tony Chen <mzzzhunter@gmail.com>
2026-07-25 11:38:28 +1200

    Align package version and include RawFileReader DLLs

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
-from setuptools import setup, find_packages
+from glob import glob
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+ROOT = Path(__file__).parent
+VERSION = "2.0.0"
+DLL_DIR = ROOT / "libs" / "Net8" / "Assemblies"
 
 setup(
     name="rawfilereader-python",
-    version="1.0.0",
+    version=VERSION,
     description="Python adapter for Thermo Fisher Scientific RawFileReader .NET assemblies",
-    long_description=open("README.md").read(),
+    long_description=(ROOT / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests*", "examples*"]),
+    data_files=[
+        (
+            "libs/Net8/Assemblies",
+            glob(str(DLL_DIR / "*.dll")),
+        )
+    ],
     python_requires=">=3.8",
     install_requires=[
         "pythonnet>=3.0.3",
     ],
+    extras_require={
+        "test": ["pytest>=7", "pytest-cov>=4"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
-        "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
